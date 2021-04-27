59a7c3968389dcfa8921df3c17e2cf30b0ca5d39
Alexandr tataritsky <luckonly@gmail.com>
2021-04-27 19:04:27 +0300

    Deleted class Opertator

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,13 +7,9 @@ public class Main {
 
         Thread callCenterThread = new Thread(null, callCenter, "Call center");
 
-        Operator operator1 = new Operator(callCenter);
-        Operator operator2 = new Operator(callCenter);
-        Operator operator3 = new Operator(callCenter);
-
-        Thread operator1Thread = new Thread(null, operator1, "Operator 1");
-        Thread operator2Thread = new Thread(null, operator2, "Operator 2");
-        Thread operator3Thread = new Thread(null, operator3, "Operator 3");
+        Thread operator1Thread = new Thread(null, callCenter::answerCall, "Operator 1");
+        Thread operator2Thread = new Thread(null, callCenter::answerCall, "Operator 2");
+        Thread operator3Thread = new Thread(null, callCenter::answerCall, "Operator 3");
 
         try {
             callCenterThread.start();
